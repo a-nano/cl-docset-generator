37cc04ab529458c2a7a51ec171dd259f6d7b21ee
Akihide Nano <an74abc@gmail.com>
2018-05-16 11:22:21 +0900

    added tests

--- cl-docset-generator-test.asd
+++ cl-docset-generator-test.asd
@@ -2,20 +2,15 @@
   This file is a part of lodo project.
 |#
 
-(in-package :cl-user)
-(defpackage lodo-test-asd
-  (:use :cl :asdf))
-(in-package :lodo-test-asd)
-
-(defsystem lodo-test
+(defsystem cl-docset-generator-test
   :author "Masashi Iizuka <liquidz.uo@gmail.com>"
   :license "MIT"
-  :depends-on (:lodo
+  :depends-on (:cl-docset-generator
                :prove)
   :components ((:module "t"
                 :components
                 ((:test-file "lodo"))))
-  :description "Test system for lodo"
+  :description "Test system for cl-docset-generator"
 
   :defsystem-depends-on (:prove-asdf)
   :perform (test-op :after (op c)
--- t/lodo.lisp
+++ t/lodo.lisp
@@ -7,8 +7,58 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :docset-generator)' in your Lisp.
 
-(plan nil)
+(setf *enable-colors* nil)
 
-;; blah blah blah.
+(defun test-dir ()
+  (namestring
+   (merge-pathnames
+    "t/tmp"
+    (asdf:component-pathname
+     (asdf:find-system "cl-docset-generator")))))
+   
+
+(plan 4)
+
+(subtest "make-docset test"
+  (let ((docset (make-docset :id "foo"
+			     :name "bar"
+			     :family "baz")))
+    (is (type-of docset) 'docset-generator::docset) 
+    (is (slot-value docset 'docset-generator::id) "foo")
+    (is (slot-value docset 'docset-generator::name) "bar")
+    (is (slot-value docset 'docset-generator::family) "baz")
+    (is (slot-value docset 'docset-generator::records) nil)
+    (is (slot-value docset 'docset-generator::icon) nil)
+    (is (slot-value docset 'docset-generator::base-dir) "/tmp")))
+
+(subtest "make-record test"
+  (let ((record (make-record :name "foo"
+			     :type "bar"
+			     :prefix "baz"
+			     :body "blah")))
+    (is (type-of record) 'docset-generator::docset-record)
+    (is (slot-value record 'docset-generator::name) "foo")
+    (is (slot-value record 'docset-generator::type) "bar")
+    (is (slot-value record 'docset-generator::prefix) "baz")
+    (is (slot-value record 'docset-generator::body) "blah")))
+
+(subtest "add-record test"
+  (let ((docset (make-docset))
+	(record (make-record)))
+    (add-record docset record)
+    (is (type-of (car (slot-value docset 'docset-generator::records)))
+	'docset-generator::docset-record)))
+
+(subtest "generate test"
+  (let ((docset (make-docset :id "foo"
+			     :name "foo"
+			     :family "foo"
+			     :base-dir (test-dir))))
+    (add-record docset
+		(make-record :name "bar"
+			     :type "Guide"
+			     :prefix "foo"
+			     :body "hello"))
+    (pass (generate docset))))
 
 (finalize)
